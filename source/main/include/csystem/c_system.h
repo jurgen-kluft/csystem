e5bbcf8b444dda7488006ad3d876d8b7907f5bd0
jurgen.kluft <jurgen.kluft@gmail.com>
2023-04-16 16:31:20 +0800

    * unittest; large refactor

--- source/main/include/csystem/c_system.h
+++ source/main/include/csystem/c_system.h
@@ -2,145 +2,141 @@
 #define __CSYSTEM_H__
 #include "cbase/c_target.h"
 #ifdef USE_PRAGMA_ONCE
-#pragma once
+#    pragma once
 #endif
 
 namespace ncore
 {
-	class alloc_t;
+    class alloc_t;
 
-	namespace nsystem
-	{
-		struct ctxt_t;
+    class system_t
+    {
+    public:
+        struct instance_t;
 
-		enum ELanguage
-		{
-			LANGUAGE_INVALID		= -1,
-			LANGUAGE_ENGLISH		= 0,
-			LANGUAGE_FRENCH			= 1,
-			LANGUAGE_ITALIAN		= 2,
-			LANGUAGE_GERMAN			= 3,
-			LANGUAGE_SPANISH		= 4,
-			LANGUAGE_GREEK			= 5,
-			LANGUAGE_ENGLISH_US		= 6,
-			LANGUAGE_FRENCH_US		= 7,
-			LANGUAGE_PORTUGUESE		= 8,
-			LANGUAGE_BRAZILIAN		= 9,			///< Brazilian Portuguese
-			LANGUAGE_JAPANESE		= 10,
-			LANGUAGE_KOREAN         = 11,			///< Korean
-			LANGUAGE_RUSSIAN        = 12,			///< Russian
-			LANGUAGE_DUTCH			= 13,
-			LANGUAGE_CHINESE_T		= 14,
-			LANGUAGE_CHINESE_S		= 15,
-			LANGUAGE_FINNISH		= 16,
-			LANGUAGE_SWEDISH		= 17,
-			LANGUAGE_DANISH			= 18,
-			LANGUAGE_NORWEGIAN		= 19,
-			LANGUAGE_POLISH			= 20,
+        static void      setSystem(system_t* instance);
+        static system_t* getSystem();
 
-			LANGUAGE_COUNT,
+        enum ELanguage
+        {
+            LANGUAGE_INVALID    = -1,
+            LANGUAGE_ENGLISH    = 0,
+            LANGUAGE_FRENCH     = 1,
+            LANGUAGE_ITALIAN    = 2,
+            LANGUAGE_GERMAN     = 3,
+            LANGUAGE_SPANISH    = 4,
+            LANGUAGE_GREEK      = 5,
+            LANGUAGE_ENGLISH_US = 6,
+            LANGUAGE_FRENCH_US  = 7,
+            LANGUAGE_PORTUGUESE = 8,
+            LANGUAGE_BRAZILIAN  = 9, ///< Brazilian Portuguese
+            LANGUAGE_JAPANESE   = 10,
+            LANGUAGE_KOREAN     = 11, ///< Korean
+            LANGUAGE_RUSSIAN    = 12, ///< Russian
+            LANGUAGE_DUTCH      = 13,
+            LANGUAGE_CHINESE_T  = 14,
+            LANGUAGE_CHINESE_S  = 15,
+            LANGUAGE_FINNISH    = 16,
+            LANGUAGE_SWEDISH    = 17,
+            LANGUAGE_DANISH     = 18,
+            LANGUAGE_NORWEGIAN  = 19,
+            LANGUAGE_POLISH     = 20,
 
-			LANGUAGE_DEFAULT		= LANGUAGE_ENGLISH,
-			LANGUAGE_MAIN			= LANGUAGE_DEFAULT
-		};
-		extern const char*	gToString(ELanguage);
+            LANGUAGE_COUNT,
 
-		enum EConsoleType
-		{
-			CONSOLE_DESKTOP,
-			CONSOLE_DEVKIT,
-			CONSOLE_TESTKIT,
-			CONSOLE_RETAIL,
-		};
+            LANGUAGE_DEFAULT = LANGUAGE_ENGLISH,
+            LANGUAGE_MAIN    = LANGUAGE_DEFAULT
+        };
 
-		enum EMediaType
-		{
-			MEDIA_LOCAL   = 0x01,
-			MEDIA_NETWORK = 0x02,
-			MEDIA_CD = 0x04,
-			MEDIA_DVD = 0x08,
-			MEDIA_BLURAY = 0x10,
-			MEDIA_SD = 0x20, 
-			MEDIA_HDD = 0x40,
-			MEDIA_SSD = 0x80
-		};
+        enum EConsoleType
+        {
+            CONSOLE_DESKTOP,
+            CONSOLE_DEVKIT,
+            CONSOLE_TESTKIT,
+            CONSOLE_RETAIL,
+        };
 
-		enum EDataSource
-		{
-			DATA_FROM_HOST = 0x00,
-			DATA_FROM_DVD = 0x10,
-			DATA_FROM_NETWORK = 0x20,
-			DATA_FROM_HARDDISK = 0x40,
-			DATA_FROM_SSD = 0x80,
-			DATA_FROM_MASK = 0xFF,
-		};
+        enum EMediaType
+        {
+            MEDIA_LOCAL   = 0x01,
+            MEDIA_NETWORK = 0x02,
+            MEDIA_CD      = 0x04,
+            MEDIA_DVD     = 0x08,
+            MEDIA_BLURAY  = 0x10,
+            MEDIA_SD      = 0x20,
+            MEDIA_HDD     = 0x40,
+            MEDIA_SSD     = 0x80
+        };
 
-		enum EEnvMode
-		{
-			ENVIRONMENT_OPEN = 0,
-			ENVIRONMENT_DEBUG = 1,
-			ENVIRONMENT_DEVELOPMENT = 2,
-			ENVIRONMENT_QA = 3,
-			ENVIRONMENT_RETAIL = 4,
-		};
+        enum EDataSource
+        {
+            DATA_FROM_HOST     = 0x00,
+            DATA_FROM_DVD      = 0x10,
+            DATA_FROM_NETWORK  = 0x20,
+            DATA_FROM_HARDDISK = 0x40,
+            DATA_FROM_SSD      = 0x80,
+            DATA_FROM_MASK     = 0xFF,
+        };
 
-		struct MAC_t
-		{
-			u8	mID[6];
-		};
+        enum EEnvMode
+        {
+            ENVIRONMENT_OPEN        = 0,
+            ENVIRONMENT_DEBUG       = 1,
+            ENVIRONMENT_DEVELOPMENT = 2,
+            ENVIRONMENT_QA          = 3,
+            ENVIRONMENT_RETAIL      = 4,
+        };
 
-		void					init					(alloc_t*, ctxt_t*& );
-		void 					update					(ctxt_t* );
-		void					shutdown				(ctxt_t* );
+        void init(alloc_t*);
+        void update();
+        void shutdown();
 
-		void					printCpuInfo			(ctxt_t* );
-		void					printMemoryInfo			(ctxt_t* );
+        bool isLittleEndian() const;
+        bool isBigEndian() const;
+        bool is64BitOS() const;
 
-		EConsoleType			getConsoleType			(ctxt_t*);
-		EMediaType				getMediaType			(ctxt_t*);
-		EDataSource				getDataSource			(ctxt_t*);
-		EEnvMode				getEnvMode				(ctxt_t*);
+        EConsoleType getConsoleType() const;
+        EMediaType   getMediaType() const;
+        EDataSource  getDataSource() const;
+        EEnvMode     getEnvMode() const;
 
-		MAC_t					getMAC					(ctxt_t*);
+        ELanguage   getLanguage() const;
+        const char* getLanguageString(ELanguage) const;
+        void        setLanguage(ELanguage language);
 
-		ELanguage				getLanguage				(ctxt_t*);
-		void					setLanguage				(ctxt_t*, ELanguage language );
+        const char* getUnixEndLine();
+        const char* getWindowsEndLine();
+        const char* getEndLine();
 
-		void					getNickname				(ctxt_t*, char* szBuffer, u32 szBufferMaxLen );
-		bool					isCircleButtonBack		(ctxt_t*);
+        void setUserName(const char* szUserName);
+        void getUserName(char* szBuffer, u32 szBufferMaxLen) const;
 
-		void					setAppTitle				(ctxt_t*, const char* title );
-		const char*				getAppTitle				(ctxt_t* );
-		const char*				getExePath				(ctxt_t* );
+        bool isCircleButtonBack() const;
 
-		bool					hasVirtualMemory		(ctxt_t* );
+        void        setAppTitle(const char* title) const;
+        const char* getAppTitle() const;
+        const char* getExePath() const;
 
-		s32						getNumCores				(ctxt_t*);
-		s32						getNumHwThreadsPerCore	(ctxt_t*);
-		u64						getCoreClockFrequency	(ctxt_t*);
+        bool hasVirtualMemory() const;
 
-		const char*				getPlatformName			(ctxt_t*);		// Name of platform
-		const char*				getBuildConfigName		(ctxt_t*);		// Debug, Release, Final
-		const char*				getBuildModeName		(ctxt_t*);		// Dev, Client, Retail
-		const char*				getConsoleTypeName		(ctxt_t*);		// 
-		const char*				getMediaTypeName		(ctxt_t*);		// 
-		const char*				getLanguageName			(ctxt_t*);
+        s32 getNumCores() const;
+        s32 getNumHwThreadsPerCore() const;
+        u64 getCoreClockFrequency() const;
 
-		u64						getCodeSegmentSize		(ctxt_t*);
-		u64						getBssSegmentSize		(ctxt_t*);
-		u64						getDataSegmentSize		(ctxt_t*);
-		u64						getMainThreadStackSize	(ctxt_t*);
-	};
-};
+        const char* getPlatformName() const;    // Name of platform
+        const char* getBuildConfigName() const; // Debug, Release, Final
+        const char* getBuildModeName() const;   // Dev, Client, Retail
+        const char* getConsoleTypeName() const; //
+        const char* getMediaTypeName() const;   //
+        const char* getLanguageName() const;
 
-#if defined TARGET_PC && defined PLATFORM_32BIT
-	#include "csystem/private/c_system_win32.h"
-#elif defined TARGET_PC && defined PLATFORM_64BIT
-	#include "csystem/private/c_system_win64.h"
-#elif defined TARGET_OSX
-	#include "csystem/private/c_system_osx.h"
-#else
-	#error "Current platform is not supported!" 
-#endif
+        u64 getCodeSegmentSize() const;
+        u64 getBssSegmentSize() const;
+        u64 getDataSegmentSize() const;
+        u64 getMainThreadStackSize() const;
 
-#endif	// __CSYSTEM_H__
+        instance_t* m_instance;
+    };
+}; // namespace ncore
+
+#endif // __CSYSTEM_H__
--- source/test/cpp/test_main.cpp
+++ source/test/cpp/test_main.cpp
@@ -1,118 +1,108 @@
-#include "xbase/x_base.h"
-#include "xbase/x_allocator.h"
-#include "xbase/x_console.h"
-#include "xsystem/x_system.h"
-#include "xunittest/xunittest.h"
-#include "xunittest/private/ut_ReportAssert.h"
+#include "cbase/c_base.h"
+#include "cbase/c_allocator.h"
+#include "cbase/c_console.h"
+#include "csystem/c_system.h"
 
-UNITTEST_SUITE_LIST(xCoreUnitTest);
+#include "cunittest/cunittest.h"
 
-UNITTEST_SUITE_DECLARE(xCoreUnitTest, cpu_info);
-UNITTEST_SUITE_DECLARE(xCoreUnitTest, exe_info);
-UNITTEST_SUITE_DECLARE(xCoreUnitTest, basic_info);
+UNITTEST_SUITE_LIST(cUnitTest);
 
-namespace xcore
+UNITTEST_SUITE_DECLARE(cUnitTest, cpu_info);
+UNITTEST_SUITE_DECLARE(cUnitTest, exe_info);
+UNITTEST_SUITE_DECLARE(cUnitTest, basic_info);
+
+namespace ncore
 {
-	// Our own assert handler
-	class UnitTestAssertHandler : public xcore::x_asserthandler
-	{
-	public:
-		UnitTestAssertHandler()
-		{
-			NumberOfAsserts = 0;
-		}
+    // Our own assert handler
+    class UnitTestAssertHandler : public ncore::asserthandler_t
+    {
+    public:
+        UnitTestAssertHandler() { NumberOfAsserts = 0; }
 
-		virtual xcore::xbool	HandleAssert(u32& flags, const char* fileName, s32 lineNumber, const char* exprString, const char* messageString)
-		{
-			UnitTest::reportAssert(exprString, fileName, lineNumber);
-			NumberOfAsserts++;
-			return false;
-		}
+        virtual bool handle_assert(u32& flags, const char* fileName, s32 lineNumber, const char* exprString, const char* messageString)
+        {
+            UnitTest::reportAssert(exprString, fileName, lineNumber);
+            NumberOfAsserts++;
+            return false;
+        }
 
+        ncore::s32 NumberOfAsserts;
+    };
 
-		xcore::s32		NumberOfAsserts;
-	};
+    class UnitTestAllocator : public UnitTest::TestAllocator
+    {
+    public:
+        ncore::alloc_t* mAllocator;
+        int             mNumAllocations;
 
-	class UnitTestAllocator : public UnitTest::Allocator
-	{
-		xcore::x_iallocator*	mAllocator;
-	public:
-						UnitTestAllocator(xcore::x_iallocator* allocator)	{ mAllocator = allocator; }
-		virtual void*	Allocate(xsize_t size)								{ return mAllocator->allocate((u32)size, 4); }
-		virtual void	Deallocate(void* ptr)								{ mAllocator->deallocate(ptr); }
-	};
+        UnitTestAllocator(ncore::alloc_t* allocator)
+            : mAllocator(allocator)
+            , mNumAllocations(0)
+        {
+        }
 
-	class TestAllocator : public x_iallocator
-	{
-		x_iallocator*		mAllocator;
-	public:
-							TestAllocator(x_iallocator* allocator) : mAllocator(allocator) { }
+        virtual void* Allocate(unsigned int size, unsigned int alignment)
+        {
+            mNumAllocations++;
+            return mAllocator->allocate(size, alignment);
+        }
+        virtual unsigned int Deallocate(void* ptr)
+        {
+            --mNumAllocations;
+            return mAllocator->deallocate(ptr);
+        }
+    };
 
-		virtual const char*	name() const										{ return "xbase unittest test heap allocator"; }
+    class TestAllocator : public alloc_t
+    {
+        UnitTest::TestAllocator* mAllocator;
 
-		virtual void*		allocate(xsize_t size, u32 alignment)
-		{
-			UnitTest::IncNumAllocations();
-			return mAllocator->allocate(size, alignment);
-		}
+    public:
+        TestAllocator(UnitTestAllocator* allocator)
+            : mAllocator(allocator)
+        {
+        }
 
-		virtual void*		reallocate(void* mem, xsize_t size, u32 alignment)
-		{
-			if (mem==NULL)
-				return allocate(size, alignment);
-			else 
-				return mAllocator->reallocate(mem, size, alignment);
-		}
+        virtual void* v_allocate(u32 size, u32 alignment) { return mAllocator->Allocate(size, alignment); }
 
-		virtual void		deallocate(void* mem)
-		{
-			UnitTest::DecNumAllocations();
-			mAllocator->deallocate(mem);
-		}
+        virtual u32 v_deallocate(void* mem) { return mAllocator->Deallocate(mem); }
 
-		virtual void		release()
-		{
-			mAllocator->release();
-			mAllocator = NULL;
-		}
-	};
-}
+        virtual void v_release()
+        {
+            // Do nothing
+        }
+    };
+} // namespace ncore
 
-xcore::x_iallocator* gTestAllocator = NULL;
-xcore::UnitTestAssertHandler gAssertHandler;
-
-xcore::xsystem::xctxt* gCtxt = NULL;
-
-bool gRunUnitTest(UnitTest::TestReporter& reporter)
+bool gRunUnitTest(UnitTest::TestReporter& reporter, UnitTest::TestContext& context)
 {
-	xbase::x_Init();
+    cbase::init();
 
 #ifdef TARGET_DEBUG
-	xcore::x_asserthandler::sRegisterHandler(&gAssertHandler);
+    ncore::UnitTestAssertHandler assertHandler;
+    ncore::context_t::set_assert_handler(&assertHandler);
 #endif
+    ncore::console->write("Configuration: ");
+    ncore::console->setColor(ncore::console_t::YELLOW);
+    ncore::console->writeLine(TARGET_FULL_DESCR_STR);
+    ncore::console->setColor(ncore::console_t::NORMAL);
 
-	xcore::x_iallocator* systemAllocator = xcore::x_iallocator::get_default();
-	xcore::UnitTestAllocator unittestAllocator( systemAllocator );
-	UnitTest::SetAllocator(&unittestAllocator);
-	
-	xcore::xconsole::write("Configuration: ");
-	xcore::xconsole::writeLine(TARGET_FULL_DESCR_STR);
+    ncore::alloc_t*          systemAllocator = ncore::context_t::system_alloc();
+    ncore::UnitTestAllocator unittestAllocator(systemAllocator);
+    context.mAllocator = &unittestAllocator;
 
-	xcore::TestAllocator testAllocator(systemAllocator);
-	gTestAllocator = &testAllocator;
+    ncore::TestAllocator testAllocator(&unittestAllocator);
+    ncore::context_t::set_system_alloc(&testAllocator);
 
-	xcore::xsystem::init(gTestAllocator, gCtxt);
+    int r = UNITTEST_SUITE_RUN(context, reporter, cUnitTest);
+    if (unittestAllocator.mNumAllocations != 0)
+    {
+        reporter.reportFailure(__FILE__, __LINE__, "cunittest", "memory leaks detected!");
+        r = -1;
+    }
 
-	int r = UNITTEST_SUITE_RUN(reporter, xCoreUnitTest);
+    ncore::context_t::set_system_alloc(systemAllocator);
 
-	xcore::xsystem::shutdown(gCtxt);
-	gCtxt = NULL;
-
-	gTestAllocator->release();
-	gTestAllocator = NULL;
-
-	UnitTest::SetAllocator(NULL);
-
-	xbase::x_Exit();
-	return r==0;
+    cbase::exit();
+    return r == 0;
 }
